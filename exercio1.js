e59bbcc93d84750947ce294eb9793550fc523ad0
Bruno Weber <128698720+BrunoErd@users.noreply.github.com>
2023-09-05 12:00:17 -0300

    Add files via upload

---
+++ exercio1.js
@@ -0,0 +1,21 @@
+var num1=458;
+var num2=546;
+
+//console.log(num1+num2)
+
+var soma;
+var resultado;
+
+function calcularSoma(nun1, nun2) {
+   soma = nun1 + nun2
+  return soma;
+}
+
+resultado = calcularSoma(num1, num2);
+console.log('A soma é:',resultado);
+
+num1 = 547
+num2 = 43554
+
+resultado = calcularSoma(num1, num2)
+console.log('A soma é:',resultado);
